30bc22adb66140e7db2adc5d6af402b7bb37920f
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-06-29 16:11:02 +0200

    refacto voice record IPG

--- app/javascript/controllers/voice_record_controller.js
+++ app/javascript/controllers/voice_record_controller.js
@@ -1,98 +1,348 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="voice-record"
 export default class extends Controller {
-  static targets = ['microphone', 'fillEffect', 'voiceInstructions', 'audioRecord',
-                    'stop']
+  static targets = [
+    'microphone', 'voiceInstructions', 'stopButton', 'elapsedTimeTag',
+    'playInstructions', 'recordingControlButtonsContainer', 'audioElement'
+  ];
 
-  newRecord() {
-    this.voiceInstructionsTarget.classList.remove('hidden');
-    this.microphoneTarget.classList.add('invisible');
-    this.stopTarget.classList.remove('invisible')
-    this.audioRecordTarget.classList.remove('hidden')
+  startAudioRecording() {
 
-    this.createUserAudio()
-  }
+    console.log("Recording Audio...");
 
-  stopRecord() {
-
-    // Arrêtez l'enregistrement audio ici
-    // Vous devez avoir accès à l'objet `source` ou `stream` pour arrêter l'enregistrement
-
-    // Par exemple, si vous utilisez MediaStream, vous pouvez appeler `stop()` sur l'objet MediaStream pour arrêter l'enregistrement
-    const mediaStream = this.stream;
-    if (mediaStream && mediaStream.getTracks) {
-      mediaStream.getTracks().forEach(track => track.stop());
+    //If a previous audio recording is playing, pause it
+    let recorderAudioIsPlaying = !audioElement.paused; // the paused property tells whether the media element is paused or not
+    console.log("paused?", !recorderAudioIsPlaying);
+    if (recorderAudioIsPlaying) {
+        audioElement.pause();
     }
 
-    // Cachez le canevas de visualisation et affichez le bouton "Enregistrer" à nouveau
-    this.audioRecordTarget.classList.add('hidden');
-    this.microphoneTarget.classList.remove('invisible');
-    this.stopTarget.classList.add('invisible')
-  }
+    //start recording using the audio recording API
+    audioRecorder.start()
+        .then(() => { //on success
 
+            //store the recording start time to display the elapsed time according to it
+            audioRecordStartTime = new Date();
 
-  createUserAudio() {
-  // Obtenir l'audio (par exemple, à partir d'un microphone)
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then((stream) => {
-        // Créer un contexte audio
-        this.stream = stream
-        const audioContext = new AudioContext();
-        const source = audioContext.createMediaStreamSource(stream);
-
-        // Créer un nœud d'analyse
-        const analyser = audioContext.createAnalyser();
-        analyser.fftSize = 2048; // Taille de la FFT pour l'analyse (modifiable selon vos besoins)
-
-        // Connecter les nœuds
-        source.connect(analyser);
-        // Connectez le nœud d'analyse à la destination finale (par exemple, les haut-parleurs)
-        analyser.connect(audioContext.destination);
-
-        // Obtenir les données audio
-        const bufferLength = analyser.fftSize;
-        const dataArray = new Uint8Array(bufferLength);
-
-        // Visualiser les données audio
-        const draw = () => {
-          requestAnimationFrame(draw);
-
-          // Obtenez les données audio d'amplitude dans le domaine temporel
-          analyser.getByteTimeDomainData(dataArray);
-
-          // Dessinez votre effet de vague basé sur les données audio ici
-          // Utilisez les valeurs de dataArray pour déterminer les dimensions et les positions des vagues
-
-          // Exemple : Dessiner une vague sur un canevas HTML
-          const canvas = document.getElementById('waveCanvas');
-          const context = canvas.getContext('2d');
-          context.clearRect(0, 0, canvas.width, canvas.height);
-          context.lineWidth = 3;
-          context.strokeStyle = '#FFC300';
-          context.beginPath();
-          const sliceWidth = canvas.width * 1.0 / bufferLength;
-          let x = 0;
-          for (let i = 0; i < bufferLength; i++) {
-            const v = dataArray[i] / 128.0;
-            const y = v * canvas.height / 2;
-            if (i === 0) {
-              context.moveTo(x, y);
-            } else {
-              context.lineTo(x, y);
+            //display control buttons to offer the functionality of stop and cancel
+            handleDisplayingRecordingControlButtons();
+        })
+        .catch(error => { //on error
+            //No Browser Support Error
+            if (error.message.includes("mediaDevices API or getUserMedia method is not supported in this browser.")) {
+                console.log("To record audio, use browsers like Chrome and Firefox.");
+                displayBrowserNotSupportedOverlay();
             }
-            x += sliceWidth;
-          }
-          context.lineTo(canvas.width, canvas.height / 2);
-          context.stroke();
-        }
 
-        // Appeler la fonction draw pour commencer la visualisation
-        draw();
-      })
-      .catch(function(error) {
-        console.error('Erreur lors de l\'obtention de l\'audio :', error);
-      });
+            //Error handling structure
+            switch (error.name) {
+                case 'AbortError': //error from navigator.mediaDevices.getUserMedia
+                    console.log("An AbortError has occured.");
+                    break;
+                case 'NotAllowedError': //error from navigator.mediaDevices.getUserMedia
+                    console.log("A NotAllowedError has occured. User might have denied permission.");
+                    break;
+                case 'NotFoundError': //error from navigator.mediaDevices.getUserMedia
+                    console.log("A NotFoundError has occured.");
+                    break;
+                case 'NotReadableError': //error from navigator.mediaDevices.getUserMedia
+                    console.log("A NotReadableError has occured.");
+                    break;
+                case 'SecurityError': //error from navigator.mediaDevices.getUserMedia or from the MediaRecorder.start
+                    console.log("A SecurityError has occured.");
+                    break;
+                case 'TypeError': //error from navigator.mediaDevices.getUserMedia
+                    console.log("A TypeError has occured.");
+                    break;
+                case 'InvalidStateError': //error from the MediaRecorder.start
+                    console.log("An InvalidStateError has occured.");
+                    break;
+                case 'UnknownError': //error from the MediaRecorder.start
+                    console.log("An UnknownError has occured.");
+                    break;
+                default:
+                    console.log("An error occured with the error name " + error.name);
+            };
+        });
   }
 
+  stopAudioRecording() {
+
+    console.log("Stopping Audio Recording...");
+
+    //stop the recording using the audio recording API
+    audioRecorder.stop()
+        .then(audioAsblob => {
+            //Play recorder audio
+            playAudio(audioAsblob);
+
+            //hide recording control button & return record icon
+            handleHidingRecordingControlButtons();
+        })
+        .catch(error => {
+            //Error handling structure
+            switch (error.name) {
+                case 'InvalidStateError': //error from the MediaRecorder.stop
+                    console.log("An InvalidStateError has occured.");
+                    break;
+                default:
+                    console.log("An error occured with the error name " + error.name);
+            };
+        });
+  }
+
+  #handleDisplayingRecordingControlButtons() {
+    //Hide the microphone button that starts audio recording
+    this.microphoneTarget.style.display = "none";
+
+    //Display the recording control buttons
+    this.recordingControlButtonsContainerTarget.classList.remove("hide");
+
+    //Handle the displaying of the elapsed recording time
+    handleElapsedRecordingTime();
+  }
+
+  /** Hide the displayed recording control buttons */
+  #handleHidingRecordingControlButtons() {
+    //Display the microphone button that starts audio recording
+    this.microphoneTarget.style.display = "block";
+
+    //Hide the recording control buttons
+    this.recordingControlButtonsContainerTarget.classList.add("hide");
+
+    //stop interval that handles both time elapsed and the red dot
+    clearInterval(elapsedTimeTimer);
+  }
+
+  /** Computes the elapsed recording time since the moment the function is called in the format h:m:s*/
+  #handleElapsedRecordingTime() {
+    //display inital time when recording begins
+    displayElapsedTimeDuringAudioRecording("00:00");
+
+    //create an interval that compute & displays elapsed time, as well as, animate red dot - every second
+    elapsedTimeTimer = setInterval(() => {
+        //compute the elapsed time every second
+        let elapsedTime = computeElapsedTime(audioRecordStartTime); //pass the actual record start time
+        //display the elapsed time
+        displayElapsedTimeDuringAudioRecording(elapsedTime);
+    }, 1000); //every second
+  }
+
+  #displayElapsedTimeDuringAudioRecording(elapsedTime) {
+    //1. display the passed elapsed time as the elapsed time in the elapsedTime HTML element
+    this.elapsedTimeTagTarget.innerHTML = elapsedTime;
+
+    //2. Stop the recording when the max number of hours is reached
+    if (elapsedTimeReachedMaximumNumberOfHours(elapsedTime)) {
+        stopAudioRecording();
+    }
+  }
+
+  #elapsedTimeReachedMaximumNumberOfHours(elapsedTime) {
+    //Split the elapsed time by the symbo :
+    let elapsedTimeSplitted = elapsedTime.split(":");
+
+    //Turn the maximum recording time in hours to a string and pad it with zero if less than 10
+    let maximumRecordingTimeInHoursAsString = maximumRecordingTimeInHours < 10 ? "0" + maximumRecordingTimeInHours : maximumRecordingTimeInHours.toString();
+
+    //if it the elapsed time reach hours and also reach the maximum recording time in hours return true
+    if (elapsedTimeSplitted.length === 3 && elapsedTimeSplitted[0] === maximumRecordingTimeInHoursAsString)
+        return true;
+    else //otherwise, return false
+        return false;
+  }
+
+  #audioRecord() {
+    let audioRecorder = {
+      /** Stores the recorded audio as Blob objects of audio data as the recording continues*/
+      audioBlobs: [],/*of type Blob[]*/
+      /** Stores the reference of the MediaRecorder instance that handles the MediaStream when recording starts*/
+      mediaRecorder: null, /*of type MediaRecorder*/
+      /** Stores the reference to the stream currently capturing the audio*/
+      streamBeingCaptured: null, /*of type MediaStream*/
+      /** Start recording the audio
+       * @returns {Promise} - returns a promise that resolves if audio recording successfully started
+       */
+      start: function () {
+          //Feature Detection
+          if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+              //Feature is not supported in browser
+              //return a custom error
+              return Promise.reject(new Error('mediaDevices API or getUserMedia method is not supported in this browser.'));
+          }
+
+          else {
+              //Feature is supported in browser
+
+              //create an audio stream
+              return navigator.mediaDevices.getUserMedia({ audio: true }/*of type MediaStreamConstraints*/)
+                  //returns a promise that resolves to the audio stream
+                  .then(stream /*of type MediaStream*/ => {
+
+                      //save the reference of the stream to be able to stop it when necessary
+                      audioRecorder.streamBeingCaptured = stream;
+
+                      //create a media recorder instance by passing that stream into the MediaRecorder constructor
+                      audioRecorder.mediaRecorder = new MediaRecorder(stream); /*the MediaRecorder interface of the MediaStream Recording
+                      API provides functionality to easily record media*/
+
+                      //clear previously saved audio Blobs, if any
+                      audioRecorder.audioBlobs = [];
+
+                      //add a dataavailable event listener in order to store the audio data Blobs when recording
+                      audioRecorder.mediaRecorder.addEventListener("dataavailable", event => {
+                          //store audio Blob object
+                          audioRecorder.audioBlobs.push(event.data);
+                      });
+
+                      //start the recording by calling the start method on the media recorder
+                      audioRecorder.mediaRecorder.start();
+                  });
+
+              /* errors are not handled in the API because if its handled and the promise is chained, the .then after the catch will be executed*/
+          }
+      },
+      /** Stop the started audio recording
+       * @returns {Promise} - returns a promise that resolves to the audio as a blob file
+       */
+      stop: function () {
+          //return a promise that would return the blob or URL of the recording
+          return new Promise(resolve => {
+              //save audio type to pass to set the Blob type
+              let mimeType = audioRecorder.mediaRecorder.mimeType;
+
+              //listen to the stop event in order to create & return a single Blob object
+              audioRecorder.mediaRecorder.addEventListener("stop", () => {
+                  //create a single blob object, as we might have gathered a few Blob objects that needs to be joined as one
+                  let audioBlob = new Blob(audioRecorder.audioBlobs, { type: mimeType });
+
+                  //resolve promise with the single audio blob representing the recorded audio
+                  resolve(audioBlob);
+              });
+              audioRecorder.cancel();
+          });
+      },
+      /** Cancel audio recording*/
+      cancel: function () {
+          //stop the recording feature
+          audioRecorder.mediaRecorder.stop();
+
+          //stop all the tracks on the active stream in order to stop the stream
+          audioRecorder.stopStream();
+
+          //reset API properties for next recording
+          audioRecorder.resetRecordingProperties();
+      },
+      /** Stop all the tracks on the active stream in order to stop the stream and remove
+       * the red flashing dot showing in the tab
+       */
+      stopStream: function () {
+          //stopping the capturing request by stopping all the tracks on the active stream
+          audioRecorder.streamBeingCaptured.getTracks() //get all tracks from the stream
+              .forEach(track /*of type MediaStreamTrack*/ => track.stop()); //stop each one
+      },
+      /** Reset all the recording properties including the media recorder and stream being captured*/
+      resetRecordingProperties: function () {
+          audioRecorder.mediaRecorder = null;
+          audioRecorder.streamBeingCaptured = null;
+
+          /*No need to remove event listeners attached to mediaRecorder as
+          If a DOM element which is removed is reference-free (no references pointing to it), the element itself is picked
+          up by the garbage collector as well as any event handlers/listeners associated with it.
+          getEventListeners(audioRecorder.mediaRecorder) will return an empty array of events.*/
+      }
+    }
+    return audioRecorder
+  }
 }
+
+//   newRecord() {
+//     this.voiceInstructionsTarget.classList.remove('hidden');
+//     this.microphoneTarget.classList.add('invisible');
+//     this.stopTarget.classList.remove('invisible');
+//     this.audioRecordTarget.classList.remove('hidden');
+
+//     this.createUserAudio();
+//   }
+
+//   stopRecord() {
+//     const mediaStream = this.stream;
+//     if (mediaStream && mediaStream.getTracks) {
+//       mediaStream.getTracks().forEach(track => track.stop());
+//     }
+
+//     this.audioRecordTarget.classList.add('hidden');
+//     this.playTarget.classList.remove('invisible');
+//     this.voiceInstructionsTarget.classList.add('hidden');
+//     this.iconsContainerTarget.classList.add('hidden');
+//     this.playInstructionsTarget.classList.remove('hidden');
+//   }
+
+//   playRecord() {
+//     const audioURL = URL.createObjectURL(this.recordedBlob);
+//     const audio = new Audio(audioURL);
+//     audio.play();
+//   }
+
+//   createUserAudio() {
+//     navigator.mediaDevices.getUserMedia({ audio: true })
+//       .then((stream) => {
+//         this.stream = stream;
+//         const audioContext = new AudioContext();
+//         const source = audioContext.createMediaStreamSource(stream);
+//         const analyser = audioContext.createAnalyser();
+//         analyser.fftSize = 16384;
+//         source.connect(analyser);
+//         analyser.connect(audioContext.destination);
+//         const bufferLength = analyser.fftSize;
+//         const dataArray = new Uint8Array(bufferLength);
+
+//         const draw = () => {
+//           requestAnimationFrame(draw);
+//           analyser.getByteTimeDomainData(dataArray);
+//           const canvas = document.getElementById('waveCanvas');
+//           const context = canvas.getContext('2d');
+//           context.clearRect(0, 0, canvas.width, canvas.height);
+//           context.lineWidth = 3;
+//           context.strokeStyle = '#FFC300';
+//           context.beginPath();
+//           const sliceWidth = canvas.width * 1.0 / bufferLength;
+//           let x = 0;
+//           for (let i = 0; i < bufferLength; i++) {
+//             const v = dataArray[i] / 128.0;
+//             const y = v * canvas.height / 2;
+//             if (i === 0) {
+//               context.moveTo(x, y);
+//             } else {
+//               context.lineTo(x, y);
+//             }
+//             x += sliceWidth;
+//           }
+//           context.lineTo(canvas.width, canvas.height / 2);
+//           context.stroke();
+//         };
+
+//         draw();
+//         this.startRecording(stream);
+//       })
+//       .catch((error) => {
+//         console.error('Erreur lors de l\'obtention de l\'audio :', error);
+//       });
+//   }
+
+//   startRecording(stream) {
+//     const mediaRecorder = new MediaRecorder(stream);
+//     const chunks = [];
+
+//     mediaRecorder.addEventListener('dataavailable', (event) => {
+//       chunks.push(event.data);
+//     });
+
+//     mediaRecorder.addEventListener('stop', () => {
+//       this.recordedBlob = new Blob(chunks, { type: 'audio/wav' });
+//     });
+
+//     mediaRecorder.start();
+//     this.mediaRecorder = mediaRecorder;
+//   }
+// }
